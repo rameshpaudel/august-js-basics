935d58bfb65f685ed0b83da000870878d7ae8cdc
Ramesh Paudel <rpaudel58@gmail.com>
2019-08-26 17:31:59 +0545

    General functions

---
+++ js/userQuery.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+    return a + b;
+}
+
+
+// Get the first value from the user
+var firstValue = prompt("Enter the first value")
+// Get the second value from the user
+var secondValue = prompt("Enter the second value")
+
+///Check if both of the given values are numeric
+
+var parsedFNum = parseInt(firstValue);
+var parsedSNum = parseInt(secondValue);
+
+if (!isNaN(parsedFNum) || !isNaN(parsedSNum)) {
+    console.log(
+        add(parsedFNum, parsedSNum)
+    )
+} else {
+    alert("Sorry your input doesn't seem to be a number")
+}
+//If its numeric return the sum
+
+
